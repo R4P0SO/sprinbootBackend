8458ad42d9bc8da94bd19d3684c64cb19a6f8b4c
R4P0SO <alexandreraposocasas@gmail.com>
2022-04-05 11:53:29 +0200

    save product on disk extension

--- src/main/java/com/example/demo/controllers/Ejercicio.java
+++ src/main/java/com/example/demo/controllers/Ejercicio.java
@@ -1,6 +1,10 @@
 package com.example.demo.controllers;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.MessageFormat;
+import java.util.Map;
 
 import com.example.demo.utils.Utils;
 
@@ -23,12 +27,18 @@ public class Ejercicio{
         long r = Math.round(Math.random()*100);
         return r + "";
     }
-    @GetMapping("/palindromo/{value}")
+    /*@GetMapping("/palindromo/{value}")
     public String palindrome(@PathVariable String value){
         boolean palindrome = Utils.isPalindrome(value);
         return palindrome ? "Si es palindromo": "No es palindromo";
        
         
+    }*/
+    // http://localhost:8080/palindromo/ana
+    @GetMapping("/palindromo/{name}")
+    public String palindrome(@PathVariable String name){
+        boolean palindrome = Utils.isPalindrome(name);
+        return palindrome ? "Si es palindromo" : "No es palindromo";
     }
     @GetMapping("/sumar")
     public String add(@RequestParam String num1, @RequestParam String num2){
@@ -36,10 +46,34 @@ public class Ejercicio{
         Object params[] = {num1, num2, resultado};
       return MessageFormat.format("la suma de {0} y {1} es {2}", params);
     }
-    /*@PostMapping("/saveProductOnDisk")
-    public String saveProductOnDisk(){
-        
-        return
-
-    }*/
-}
+    @PostMapping("/saveProductOnDisk")
+    public String saveProductOnDisk(@RequestParam Map<String,String> body){
+        //obtener los datos un producto {articulo, precio}
+        String articleValue = body.get("article");
+        String priceValue = body.get("price");
+        //valido que el articulo precio no sean vacios
+        if(articleValue.equals("")  || priceValue.equals("") ){
+            return "Error datos incorrectos";
+        }
+            if(Integer.parseInt(priceValue) < 0){
+                return "Error el precio es negativo";
+        }
+        FileWriter fw = null;
+        PrintWriter pw = null;
+        try {
+            fw = new FileWriter("datos.txt");
+            pw = new PrintWriter(fw);
+            pw.print(articleValue);
+            pw.print(",");
+            pw.print(priceValue);
+            pw.print("\n");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return "Error al escribir en disco";
+        }finally{
+            pw.close();
+        }
+        return "produc guardado correct";
+    }
+} 
--- src/main/java/com/example/demo/utils/Utils.java
+++ src/main/java/com/example/demo/utils/Utils.java
@@ -1,9 +1,17 @@
 package com.example.demo.utils;
 
-public class Utils {
+/*public class Utils {
     public static boolean isPalindrome(String value){
         StringBuilder builder = new  StringBuilder(value);
         String reversedName=builder.reverse().toString();
         return value.equals(reversedName);
     }
+}*/
+public class Utils {
+    public static boolean isPalindrome(String value){
+        StringBuilder builder = new StringBuilder(value);
+        String reversedName = builder.reverse().toString();
+        return value.equals(reversedName);
+    }
 }
+
