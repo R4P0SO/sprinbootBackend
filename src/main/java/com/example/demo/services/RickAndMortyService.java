5e76cec443f3e6cff8a2c699c382a484515e0bef
R4P0SO <alexandreraposocasas@gmail.com>
2022-04-07 12:59:57 +0200

    rickandmotry all added

--- src/main/java/com/example/demo/services/RickAndMortyService.java
+++ src/main/java/com/example/demo/services/RickAndMortyService.java
@@ -21,11 +21,21 @@ public class RickAndMortyService {
         Person person = restTemplate.getForObject(url, Person.class);
         return person;
     }
+    public ArrayList<Person> getAllCharactersFromAPI(){
+        ArrayList<Person> allPersons = new ArrayList<Person>();
+        for(int i=0;i<=42;i++){
+            String url = "https://rickandmortyapi.com/api/character?page="+i;
+            Persons persons = restTemplate.getForObject(url, Persons.class);
+            allPersons.addAll(persons.results);
+        
+        }
+        return allPersons;
+
+    }
     public ArrayList<Person> getCharactersFromAPI(){
         String url = "https://rickandmortyapi.com/api/character";
-        Persons persons=restTemplate.getForObject(url, Persons.class);
+        Persons persons = restTemplate.getForObject(url, Persons.class);
         return persons.results;
-
     }
 
 }
